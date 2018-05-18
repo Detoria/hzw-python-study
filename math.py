feb774186f408eacbb5e438b6a1d06e87c52aa7f
Detoria <huangzewei_job@163.com>
2018-05-18 19:35:42 +0800

    update math.py

--- math.py
+++ math.py
@@ -5,6 +5,7 @@ import os
 import sys
 import socket
 import commands
+import psutil
 
 if os.geteuid() != 0 :
     print ("此脚本必须以root用户身份运行")
@@ -12,21 +13,14 @@ if os.geteuid() != 0 :
 
 class Machine():
 
-    hostname = socket.gethostname()
-
     def __init__(self):
 
         self.data = {}
-        self.data['hostname'] = self.__class__.hostname
-        self.getMemStatus()
-        self.getCPUstatus()
-        self.getDiskStatus()
-        self.getNetworkStatus()
+        self.data['hostname'] = socket.gethostname()
 
     def getCPUstatus(self):
         CPU_Use = commands.getoutput("top -bn 1 -i -c |sed -n '3p' |awk -F [:] '{print $2}' |sed 's/[[:space:]]//'|sed 's/[[:space:]]//'")
         self.data['CPU'] = CPU_Use
-
 
     def getMemStatus(self):
         MemTotal = commands.getoutput("grep MemTotal /proc/meminfo | awk '{print $2}'")
@@ -62,21 +56,87 @@ class Machine():
 
     def getOpenstackServerStatus(self):
         self.data['OpenstackServer'] = {}
-        OpenstackServer = commands.getoutput("ls -l /etc/systemd/system/multi-user.target.wants/ | grep openstack | awk '{print $9}'")
-        NeutronServer = commands.getoutput("ls -l /etc/systemd/system/multi-user.target.wants/ | grep neutron | awk '{print $9}' | grep -v 'ovs'")
+        OpenstackServer = commands.getoutput("ls -l /etc/systemd/system/multi-user.target.wants/ | egrep 'openstack|neutron' | awk '{print $9}' |grep -v 'ovs'")
         status, result = commands.getstatusoutput("ls -l  /etc/systemd/system/multi-user.target.wants/ |grep openstack")
         if status != 0:
             self.data['OpenstackServer']["Server"] = None
         else:
-            for i in OpenstackServer, NeutronServer:
-                ServerStatus = commands.getoutput("systemctl status %s|grep Active |awk '{print $2}'" %i)
+            httpstatus, httpresult = self.ServerCheck('http')
+            if httpstatus == 'active':
+                self.data['OpenstackServer'][httpresult] = 'up'
+
+            for i in OpenstackServer.split():
+                ServerStatus, ServerResult = self.ServerCheck(i)
                 if ServerStatus == 'active':
-                    print "OK"
+                    self.data['OpenstackServer'][i] = 'up'
+                else:
+                    self.data['OpenstackServer'][i] = 'down'
+
+    def getOtherServerStatus(self):
+        self.data['OtherServer'] = {}
+        otherserver = ['redis.service','redis-sentinel.service', 'mariadb', 'rabbitmq', 'mongod', 'memcache', 'haproxy', 'keepalived']
+        for i in otherserver:
+            status, result = self.ServerCheck(i)
+            if status == 'active':
+                self.data['OtherServer'][result] = 'up'
+            elif status == 1:
+                self.data['OtherServer'][result] = 'None'
+            else:
+                self.data['OtherServer'][result] = 'down'
+
+    def getOpenstackSystem(self):
+        novaserver = commands.getoutput("source /root/keystonerc_admin && nova service-list |grep nova |awk -F '\|' '{print $3\" \"$4\" \"$6\" \"$7}' 2>/dev/null")
+        neutronserver = commands.getoutput("source /root/keystonerc_admin && neutron agent-list |grep gd1 |awk -F '\|' '{print $4\" \"$6\" \"$7\" \"$8}' 2>/dev/null")
+        cinderserver = commands.getoutput("source /root/keystonerc_admin && cinder service-list | grep gd1 |awk -F '\|' '{print $2\" \"$3\" \"$5\" \"$6}' 2>/dev/null")
+        user = commands.getoutput("source /root/keystonerc_admin && keystone user-list 2>/dev/null |grep 'nova\|swift\|neutron\|aodh\|ceilometer\|glance\|gnocchi\|heat\|manila\|cinder\|designate' |awk -F '\|' '{print $3" "$4}' 2>/dev/null")
+        tenant = commands.getoutput("source /root/keystonerc_admin && keystone tenant-list 2>/dev/null|grep 'admin\|services' | awk -F '\|' '{print $3" "$4}' 2>/dev/null")
 
 
+    def ServerCheck(self, server):
+        serverstatus, serverresult = commands.getstatusoutput("ls -l /etc/systemd/system/multi-user.target.wants/ |grep -w %s |awk '{print $9}'" %server)
+        if serverstatus == 0 and serverresult != '':
+            status = commands.getoutput("systemctl status %s|grep Active |awk '{print $2}'" %serverresult)
+            return status, serverresult
+        else:
+            return (1, server)
 
-machine = Machine()
+    def getCephClusterStatus(self):
+        pass
 
-machine.getOpenstackServerStatus()
-# print machine.data
+    def getQiyunResinStatus(self):
+        resinfile = '/apps/sh/resin/'
 
+        resinport = commands.getoutput("ls -l %s |grep resin |awk '{print $9}'|awk -F[_] '{print $4}'|awk -F[.] '{print $1}'" %resinfile)
+
+
+    def ProcessCheck(self, port):
+        serverPID = commands.getoutput("netstat -anlp |grep -w %s |awk '{print $7}'|awk -F '/' '{print $1}'" %port)
+        try:
+            p = psutil.Process(int(serverPID))
+            return 0
+        except Exception as e:
+            return 1
+
+
+    def get(self):
+
+        # self.getMemStatus()
+        # self.getCPUstatus()
+        # self.getDiskStatus()
+        # self.getNetworkStatus()
+        # self.getOpenstackServerStatus()
+        # self.getOtherServerStatus()
+        # if os.path.exists('/etc/ceph/ceph.client.admin.keyring')
+        #     self.getCephClusterStatus()
+        if os.path.exists('/apps/sh/resin')
+            self.getQiyunResinStatus()
+
+        return self.data
+
+if __name__ == '__main__':
+
+    machine = Machine()
+
+    # machine.getOpenstackSystem()
+    # print Machine().hostname
+    print machine.get()
